27331f06cb548d93d37e771c95b38ee04f074de6
Yago Goltara <goltarayago@gmail.com>
2023-07-04 23:55:19 -0300

    Adicionando script (run.sh) para automatizar todo o processo de criação do container, verificação de disponibilidade, teste de requisição e retirada do container do ar.

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "[=============================]"
+echo "[ Running docker-compose.yaml ]"
+echo "[=============================]"
+docker compose up -d
+printf "\n\n\n"
+
+
+sleep 2
+echo "[===========================================]"
+echo "[ Making a request to application with curl ]"
+echo "[===========================================]"
+curl http://127.0.0.1/
+printf "\n\n\n"
+
+sleep 2
+echo "[===============================]"
+echo "[ Showing the container running ]"
+echo "[===============================]"
+docker ps
+printf "\n\n\n"
+
+sleep 2
+echo    "[=======================================]"
+read -p "[ Do you wanna stop it? Enter y or n: " answer
+echo    "[=======================================]"
+printf "\n\n\n"
+
+
+if test $answer == 'y' || test $answer == 'Y'
+then
+    sleep 1
+    echo "[==============================]"
+    echo "[ Stopping docker-compose.yaml ]"
+    echo "[==============================]"
+    docker compose down
+fi
